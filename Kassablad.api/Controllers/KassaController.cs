60c54b1caf6e563d1c00e1ccbafd6911339d3dfd
Ruben <ruben.hannon@gmail.com>
2020-01-06 14:13:56 +0100

    updated controllers to be authorized

--- Kassablad.api/Controllers/KassaController.cs
+++ Kassablad.api/Controllers/KassaController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 using Kassablad.api.Models;
 using Kassablad.api.Data;
 
@@ -12,6 +13,7 @@ namespace Kassablad.api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class KassaController : ControllerBase
     {
         private readonly KassabladContext _context;
